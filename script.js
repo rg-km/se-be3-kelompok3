fceb86d3f029fa0c88434fee7450b0545e6f1218
gilangsp <gilang.putra@salt.co.id>
2022-03-13 01:21:21 +0700

    feat : create snake game abstraction

--- script.js
+++ script.js
@@ -1,1 +1,275 @@
-console.log('Hello World');
+const CANVAS_HEIGTH = 600
+const CANVAS_WIDTH = 900
+const CELL_SIZE = 20
+const MAX_HP = 3
+
+function initRandomPostition() {
+    return {
+        posX: Math.floor(Math.random() * ((CANVAS_WIDTH - CELL_SIZE) / CELL_SIZE)) * CELL_SIZE,
+        posY: Math.floor(Math.random() * ((CANVAS_HEIGTH - CELL_SIZE) / CELL_SIZE)) * CELL_SIZE,
+    }
+}
+
+function initDirection() {
+    return Math.floor(Math.random() * 4)
+}
+
+
+function getInitObject() {
+    const snake = {
+        id: 1,
+        posX: 460,
+        posY: 300,
+        direction: initDirection(),
+        body: [{ posX: 460, posY: 300 }],
+        bodyImage: document.getElementById('snakeBody'),
+        headImage: document.getElementById('snakeHead'),
+        score: 0,
+        hp: 3
+    }
+
+    const apple = {
+        posX: initRandomPostition().posX,
+        posY: initRandomPostition().posY,
+        image: document.getElementById('apple')
+    }
+
+    const apple2 = {
+        posX: initRandomPostition().posX,
+        posY: initRandomPostition().posY,
+        image: document.getElementById('apple')
+    }
+
+    const medicine = {
+        posX: initRandomPostition().posX,
+        posY: initRandomPostition().posY,
+        image: document.getElementById('medicine'),
+        isEaten: false
+    }
+    return { snake, apple, apple2, medicine }
+}
+
+function main() {
+
+    const { snake, apple, apple2, medicine } = getInitObject()
+
+    INIT_LEVEL_1(snake, apple, apple2, medicine)
+
+}
+
+function INIT_LEVEL_1(snake, apple, apple2, medicine) {
+    const snakeBoard = document.getElementById('snakeBoard');
+    const boardGame = new SnakeAPI(snakeBoard)
+
+    document.getElementById('labelLevel').textContent = "LEVEL 1"
+
+    const cactus = {
+        posX: initRandomPostition().posX,
+        posY: initRandomPostition().posY,
+        direction: initDirection(),
+        image: document.getElementById('cactus')
+    }
+
+    const cactusInterval = setInterval(() => {
+        cactus.direction = initDirection()
+    }, 2000)
+
+    boardGame.initSpeed(35)
+    boardGame.startRunner(() => {
+        boardGame.createSnake(snake)
+        boardGame.createObjectOnBoard(apple)
+        boardGame.createObjectOnBoard(apple2)
+        medicineAtPrimeNumber(boardGame, medicine, snake.score)
+        observeHealthPoint(boardGame, snake)
+        updateScore(snake)
+        updateSpeed(35)
+
+        //TODO:
+        //BUAT OBSTACLE ATAU DINDING DISINI
+        boardGame.createMovingObstacle(cactus, onHitByCactus)
+
+        if (snake.score === 5) {
+            boardGame.stopRunner()
+            boardGame.clearBoard()
+            INIT_LEVEL_2(snake, apple, apple2, medicine)
+            clearInterval(cactusInterval)
+        }
+
+    })
+}
+
+function INIT_LEVEL_2(snake, apple, apple2, medicine) {
+    const snakeBoard = document.getElementById('snakeBoard');
+    const boardGame = new SnakeAPI(snakeBoard)
+
+    document.getElementById('labelLevel').textContent = "LEVEL 2"
+
+    boardGame.initSpeed(40)
+    boardGame.startRunner(() => {
+        boardGame.createSnake(snake)
+        boardGame.createObjectOnBoard(apple)
+        boardGame.createObjectOnBoard(apple2)
+        medicineAtPrimeNumber(boardGame, medicine, snake.score)
+        observeHealthPoint(boardGame, snake)
+        updateScore(snake)
+        updateSpeed(40)
+
+        //TODO:
+        //BUAT OBSTACLE ATAU DINDING DISINI
+
+        if (snake.score === 10) {
+            boardGame.stopRunner()
+            boardGame.clearBoard()
+            INIT_LEVEL_3(snake, apple, apple2, snakeBoard, medicine)
+        }
+    })
+}
+
+function INIT_LEVEL_3(snake, apple, apple2, medicine) {
+    const snakeBoard = document.getElementById('snakeBoard');
+    const boardGame = new SnakeAPI(snakeBoard)
+
+    document.getElementById('labelLevel').textContent = "LEVEL 3"
+
+    const cactus = {
+        posX: initRandomPostition().posX,
+        posY: initRandomPostition().posY,
+        direction: initDirection(),
+        image: document.getElementById('cactus')
+    }
+
+    const cactusInterval = setInterval(() => {
+        cactus.direction = initDirection()
+    }, 2000)
+
+    boardGame.initSpeed(45)
+    boardGame.startRunner(() => {
+        boardGame.createSnake(snake)
+        boardGame.createObjectOnBoard(apple)
+        boardGame.createObjectOnBoard(apple2)
+        medicineAtPrimeNumber(boardGame, medicine, snake.score)
+        observeHealthPoint(boardGame, snake)
+        updateScore(snake)
+        updateSpeed(45)
+
+        //TODO:
+        //BUAT OBSTACLE ATAU DINDING DISINI
+        boardGame.createMovingObstacle(cactus, onHitByCactus)
+
+        if (snake.score === 15) {
+            boardGame.stopRunner()
+            boardGame.clearBoard()
+            INIT_LEVEL_4(snake, apple, apple2, snakeBoard)
+            clearInterval(cactusInterval)
+        }
+
+    })
+}
+
+function INIT_LEVEL_4(snake, apple, apple2, medicine) {
+    const snakeBoard = document.getElementById('snakeBoard');
+    const boardGame = new SnakeAPI(snakeBoard)
+
+    document.getElementById('labelLevel').textContent = "LEVEL 4"
+
+    boardGame.initSpeed(50)
+    boardGame.startRunner(() => {
+        boardGame.createSnake(snake)
+        boardGame.createObjectOnBoard(apple)
+        boardGame.createObjectOnBoard(apple2)
+        medicineAtPrimeNumber(boardGame, medicine, snake.score)
+        observeHealthPoint(boardGame, snake)
+        updateScore(snake)
+        updateSpeed(50)
+
+        //TODO:
+        //BUAT OBSTACLE ATAU DINDING DISINI
+
+        if (snake.score === 20) {
+            boardGame.stopRunner()
+            boardGame.clearBoard()
+            INIT_LEVEL_5(snake, apple, apple2, snakeBoard)
+        }
+
+    })
+}
+
+function INIT_LEVEL_5(snake, apple, apple2, medicine) {
+    const snakeBoard = document.getElementById('snakeBoard');
+    const boardGame = new SnakeAPI(snakeBoard)
+
+    document.getElementById('labelLevel').textContent = "LEVEL 5"
+
+    boardGame.initSpeed(55)
+    boardGame.startRunner(() => {
+        boardGame.createSnake(snake)
+        boardGame.createObjectOnBoard(apple)
+        boardGame.createObjectOnBoard(apple2)
+        medicineAtPrimeNumber(boardGame, medicine, snake.score)
+        observeHealthPoint(boardGame, snake)
+        updateScore(snake)
+        updateSpeed(55)
+
+        //TODO:
+        //BUAT OBSTACLE ATAU DINDING DISINI
+    })
+}
+
+function medicineAtPrimeNumber(gameAPI, medicine, score) {
+
+    if (isPrime(score)) {
+        gameAPI.createObjectOnBoard(medicine, true, onEatMedicine)
+        console.log('score', score)
+    }
+}
+
+function onEatMedicine(snake, medicine) {
+    if (snake.hp < 3) {
+        snake.hp++
+    }
+    const { posX, posY } = initRandomPostition()
+    medicine.posX = posX
+    medicine.posY = posY
+    snake.score++
+}
+
+function observeHealthPoint(game, snake) {
+    const hpStatus = document.getElementsByClassName('hp')
+    if (snake.hp === 0) {
+        game.stopRunner()
+        setTimeout(() => {
+            alert('Game Over.. You Lose..')
+            const { snake, apple, apple2, medicine } = getInitObject()
+            INIT_LEVEL_1(snake, apple, apple2, medicine)
+        }, game.getSpeed());
+    }
+
+    for (let i = 0; i < MAX_HP; i++) {
+        hpStatus[i].src = 'assets/hp-empty.png'
+        for (let j = 0; j < snake.hp; j++) {
+            hpStatus[j].src = 'assets/hp-full.png'
+        }
+    }
+}
+
+function onHitByCactus(snake, cactus) {
+    const { posX, posY } = initRandomPostition()
+    snake.body = [{ posX, posY }]
+    snake.score--
+    snake.posX = posX
+    snake.posY = posY
+}
+
+const isPrime = num => {
+    for (let i = 2, s = Math.sqrt(num); i <= s; i++)
+        if (num % i === 0) return false;
+    return num > 1;
+}
+
+function updateScore(snake){
+    document.getElementById('statusScore').textContent = snake.score
+}
+
+function updateSpeed(speed){
+    document.getElementById('statusSpeed').textContent = speed
+}
---
+++ snakeAPI.js
@@ -0,0 +1,253 @@
+const OREIENTATION_VERTICAL = 'OREIENTATION_VERTICAL'
+const OREIENTATION_HORIZONTAL = 'OREIENTATION_HORIZONTAL'
+
+const DIRECTION_LEFT = 0
+const DIRECTION_UP = 1
+const DIRECTION_RIGHT = 2
+const DIRECTION_DOWN = 3
+
+class SnakeAPI {
+    constructor(canvasNode) {
+        this.ctx = canvasNode.getContext('2d')
+        this.canvasWidth = canvasNode.width
+        this.canvasHeigth = canvasNode.height
+        this.speedRunner = 100;
+        this.snake = []
+    }
+
+    startRunner = (callback) => {
+        this.intervalDraw = setInterval(() => {
+            this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeigth);
+            callback()
+        }, this.speedRunner)
+    }
+
+    initSpeed = (speed = 100) => {
+        this.speedRunner -= speed;
+    }
+
+    getRandomPosition = () => {
+        return {
+            posX: Math.floor(Math.random() * ((this.canvasWidth - CELL_SIZE) / CELL_SIZE)) * CELL_SIZE,
+            posY: Math.floor(Math.random() * ((this.canvasHeigth - CELL_SIZE) / CELL_SIZE)) * CELL_SIZE,
+        }
+    }
+
+    stopRunner = () => {
+        clearInterval(this.intervalDraw)
+    }
+
+    getContext = () => {
+        return this.ctx
+    }
+
+    getSpeed = () => {
+        return this.speedRunner
+    }
+
+    clearBoard = () => {
+        this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeigth);
+    }
+
+    obastacle = (posX, posY, size = CELL_SIZE, orientation = OREIENTATION_HORIZONTAL, color = '#808080', onHitObstacleCallback = () => { }) => {
+        this.ctx.fillStyle = color
+
+        const { isHitObastacle, affectedSnake } = this.isHittedObastacle(posX * CELL_SIZE, posY * CELL_SIZE, size, orientation)
+        if (isHitObastacle) {
+            const { posX, posY } = this.getRandomPosition()
+            onHitObstacleCallback(affectedSnake)
+            affectedSnake.hp--;
+            affectedSnake.posX = posX
+            affectedSnake.posY = posY
+            affectedSnake.direction = initDirection()
+            affectedSnake.body = [{ posX, posY }]
+        }
+
+        if (orientation === OREIENTATION_HORIZONTAL) {
+            this.ctx.fillRect(posX * CELL_SIZE, posY * CELL_SIZE, size * CELL_SIZE, CELL_SIZE)
+        } else {
+            this.ctx.fillRect(posX * CELL_SIZE, posY * CELL_SIZE, CELL_SIZE, size * CELL_SIZE)
+        }
+
+    }
+
+    isHittedObastacle = (posX, posY, size, orientationObastacle) => {
+        let isHitObastacle = false
+        let affectedSnake
+        if (orientationObastacle === OREIENTATION_VERTICAL) {
+            for (let i = 0; i < this.snake?.length; i++) {
+                const snake = this.snake[i]
+                for (let cell = 0; cell < size; cell++) {
+                    if (snake.posX === posX && snake.posY === posY + (cell * CELL_SIZE)) {
+                        isHitObastacle = true
+                        affectedSnake = snake
+                    }
+                }
+            }
+
+        } else if (orientationObastacle === OREIENTATION_HORIZONTAL) {
+            for (let i = 0; i < this.snake?.length; i++) {
+                const snake = this.snake[i]
+                for (let cell = 0; cell < size; cell++) {
+                    if (snake.posX === posX + (cell * CELL_SIZE) && snake.posY === posY) {
+                        isHitObastacle = true
+                        affectedSnake = snake
+                    }
+                }
+            }
+        }
+        return { isHitObastacle, affectedSnake }
+    }
+
+    createObjectOnBoard = (object, isBlinking, overideOnHitObject) => {
+        const { isHitted, affectedSnake } = this.isHittedCell(object)
+        if (isBlinking) {
+            blinking(() => {
+                this.ctx.drawImage(object.image, object.posX, object.posY)
+            })
+        } else {
+            this.ctx.drawImage(object.image, object.posX, object.posY)
+        }
+
+        if (isHitted) {
+            if (overideOnHitObject) {
+                overideOnHitObject(affectedSnake, object)
+                return
+            }
+            object.posX = this.getRandomPosition().posX
+            object.posY = this.getRandomPosition().posY
+            affectedSnake.score += 1
+            affectedSnake.body.push({ posX: affectedSnake.posX, posY: affectedSnake.posY })
+        }
+    }
+
+    createMovingObstacle = (object, callbackOnHittedMovingObject = () => { }) => {
+        this.ctx.drawImage(object.image, object.posX, object.posY)
+        this.movingObject(object)
+        this.movingPortal(object)
+
+        for (let i = 0; i < this.snake?.length; i++) {
+            const snake = this.snake[i]
+            for (let j = 0; j < snake.body.length; j++) {
+                const bodySnake = snake.body[j]
+                if (bodySnake.posX === object.posX && bodySnake.posY === object.posY) {
+                    callbackOnHittedMovingObject(snake, object)
+                    const { posX, posY } = this.getRandomPosition()
+                    snake.hp--;
+                    snake.body = [{ posX, posY }]
+                    snake.posX = posX
+                    snake.posY = posY
+                    snake.direction = initDirection()
+                }
+            }
+        }
+    }
+
+    isHittedCell = (objectCell) => {
+        let isHitted = false
+        let affectedSnake;
+        for (let i = 0; i < this.snake?.length; i++) {
+            const snake = this.snake[i]
+            if (snake.posX === objectCell.posX && snake.posY === objectCell.posY) {
+                isHitted = true
+                affectedSnake = snake
+            }
+        }
+        return { isHitted, affectedSnake }
+    }
+
+    createSnake = (snake, overrideControl) => {
+        let defaultSnakeColor = 'orange'
+        const snakeIDs = this.snake.map(it => it.id)
+        if (!snakeIDs.includes(snake.id)) {
+            this.snake.push(snake)
+        }
+
+        if (snake.color) {
+            defaultSnakeColor = snake.color
+        }
+
+        if (snake.headImage) {
+            this.ctx.drawImage(snake.headImage, snake.posX, snake.posY)
+        } else {
+            this.ctx.fillStyle = defaultSnakeColor
+            this.ctx.fillRect(snake.posX, snake.posY, CELL_SIZE, CELL_SIZE)
+        }
+
+        for (let i = 0; i < snake.body.length; i++) {
+            const body = snake.body[i]
+            if (i !== 0) {
+                if (snake.bodyImage) {
+                    this.ctx.drawImage(snake.bodyImage, body.posX, body.posY)
+                } else {
+                    this.ctx.fillRect(body.posX, body.posY, CELL_SIZE, CELL_SIZE)
+                }
+            }
+        }
+
+        // snake creeping direction
+        this.movingObject(snake)
+
+        //update body
+        snake.body.unshift({ posX: snake.posX, posY: snake.posY })
+        snake.body.pop()
+
+        //snake control creeping direction
+        document.addEventListener('keydown', (event) => {
+            if (event.code === 'ArrowUp' && snake.direction !== DIRECTION_DOWN) {
+                snake.direction = DIRECTION_UP
+            } else if (event.code === 'ArrowRight' && snake.direction !== DIRECTION_LEFT) {
+                snake.direction = DIRECTION_RIGHT
+            } else if (event.code === 'ArrowDown' && snake.direction !== DIRECTION_UP) {
+                snake.direction = DIRECTION_DOWN
+            } else if (event.code === 'ArrowLeft' && snake.direction !== DIRECTION_RIGHT) {
+                snake.direction = DIRECTION_LEFT
+            }
+        })
+
+        //portal snake
+        this.movingPortal(snake)
+
+
+    }
+
+    movingObject = (object) => {
+        if (object.direction === DIRECTION_RIGHT) {
+            object.posX += 20;
+        }
+
+        if (object.direction === DIRECTION_DOWN) {
+            object.posY += 20;
+        }
+
+        if (object.direction === DIRECTION_LEFT) {
+            object.posX -= 20;
+        }
+
+        if (object.direction === DIRECTION_UP) {
+            object.posY -= 20;
+        }
+    }
+
+    movingPortal = (object) => {
+        if (object.posX > (this.canvasWidth - CELL_SIZE)) {
+            object.posX = -20
+        } else if (object.posX < 0) {
+            object.posX = this.canvasWidth
+        } else if (object.posY > (this.canvasHeigth - CELL_SIZE)) {
+            object.posY = -20
+        } else if (object.posY < 0) {
+            object.posY = this.canvasHeigth
+        }
+    }
+}
+
+let counter = 0
+function blinking(onBlink = () => { }) {
+    counter++
+    if (counter % 2 === 0) {
+        onBlink()
+    }
+}
+
+
